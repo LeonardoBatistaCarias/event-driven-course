c1ba9d2fec0936a2ef47c1f18804c655e3d84244
Leonardo Batista Carias <leonardo1carias@gmail.com>
2022-06-14 18:39:49 -0300

    Section 7 - CQRS Pattern. Product Microservice

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # event-driven-course
+
+Run Axon Server with Docker
+
+COMMAND: docker run --name axonserver -p 8024:8024 -p 8124:8124 -v "PATH/data":/data -v "PATH/eventdata":/eventdata -v "PATH/config":/config axoniq/axonserver
+
+Obs: replace "PATH" with a path to a desired folder
--- product-service/src/main/java/com/leonardobatistacarias/estore/ProductService/controller/ProductController.java
+++ product-service/src/main/java/com/leonardobatistacarias/estore/ProductService/controller/ProductController.java
@@ -1,19 +1,43 @@
 package com.leonardobatistacarias.estore.ProductService.controller;
 
+import com.leonardobatistacarias.estore.ProductService.command.CreateProductCommand;
+import com.leonardobatistacarias.estore.ProductService.model.CreateProductRestModel;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    public ProductController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
 
     @PostMapping
-    public String createProduct() {
-        return "HTTP POST Handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString()).build();
+
+        String returnValue;
+        try{
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        }catch(Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+
+        return returnValue;
     }
 
     @GetMapping
