49b6aac1e9b7b7ad2ad2a4594684733891b551d0
Leonardo Batista Carias <leonardo1carias@gmail.com>
2022-06-23 22:37:20 -0300

    OrderService microsservice created

---
+++ order-service/src/test/java/com/leonardobatistacarias/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardobatistacarias.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
